a12a3c3bddbdf4201184eae072a580992fc5ef6e
Gavin16 <kupature@163.com>
2020-12-07 00:40:54 +0800

    bodywrite 处理后对应消息转换器顺序调整

--- Boot-Demo-Web/src/main/java/com/demo/web/config/ListenerConfig.java
+++ Boot-Demo-Web/src/main/java/com/demo/web/config/ListenerConfig.java
@@ -4,7 +4,11 @@ import com.demo.web.listener.MyHttpSessionListener;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class ListenerConfig implements WebMvcConfigurer {
@@ -14,6 +18,14 @@ public class ListenerConfig implements WebMvcConfigurer {
         ServletListenerRegistrationBean srb = new ServletListenerRegistrationBean();
         srb.setListener(new MyHttpSessionListener());
         return srb;
+    }
+
+    /**
+     * 将json处理的转换器放到第一位，使得先让 json 转换器处理返回值，以免String转换器误将其它类型转为String
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new MappingJackson2HttpMessageConverter());
     }
 
 }
